e55457c9ec59bf29c10e42a1f1eade59da1673dc
rmaatzak <159541913+rmaatzak@users.noreply.github.com>
2024-11-22 16:41:00 -0300

    update

--- bla.js
+++
@@ -1,9 +0,0 @@
-// Seleciona os elementos
-const toggleButton = document.getElementById("toggleButton");
-const content = document.getElementById("content");
-
-// Adiciona um evento de clique ao botão
-toggleButton.addEventListener("click", function() {
-    // Alterna a classe "hidden" no elemento "content"
-    content.classList.toggle("hidden");
-});
